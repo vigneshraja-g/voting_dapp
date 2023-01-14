4d47bf7b56f4c2f717a3f014e198f7b0d46f46a7
vigneshraja-g <vigneshraja20@gmail.com>
2023-01-14 13:36:56 +0530

    Contract update

--- src/contracts/Election.sol
+++ src/contracts/Election.sol
@@ -2,8 +2,8 @@ pragma solidity ^0.8.0;
 
 contract Election {
     struct candidate {
-        string name;
         uint256 index;
+        string name;
         uint256 votecount;
     }
 
@@ -20,23 +20,29 @@ contract Election {
     constructor() {
         addCandidate("Canndidate 1");
         addCandidate("Canndidate 2");
-        addCandidate("Canndidate 3");
     }
 
     function addCandidate(string memory _name) private {
         candidatesCount++;
-        candidates.push(
-            candidate({id: candidatesCount, name: _name, voteCount: 0})
-        );
+        candidates[candidatesCount] = candidate(candidatesCount, _name, 0);
     }
 
-    function voting(address voter) public {
+    function voting(string memory _name, uint256 _index) public {
         require(voters[msg.sender].voted == false, "already voted");
         candidates[_index].votecount++;
         voters[msg.sender] = voter(_name, _index, true);
     }
 
-    function result(address voter) public {
-        
+    function getwinner() public view returns (uint256) {
+        uint256 x = candidates[1].votecount;
+        uint256 y = candidates[2].votecount;
+
+        if (x > y) {
+            return candidates[1].index;
+        } else if (y > x) {
+            return candidates[2].index;
+        } else {
+            return 0;
+        }
     }
 }
